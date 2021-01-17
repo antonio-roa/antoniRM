05c6eda6b1fc0fbd578a461ebc73fd4aa977b99f
antonio-roa <Antonio Roa Millan>
2021-01-17 02:25:49 -0600

    Factory y seeder listado de productos

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $productos = DB::table("productos")->get();
+    return view('welcome',[
+        'productos' => $productos 
+    ] );
 });
